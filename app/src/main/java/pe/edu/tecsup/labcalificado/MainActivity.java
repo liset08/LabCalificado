be168613bb4f6bb981d76e704c6502791bc0dad2
Linder Hassinger <linderhassinger00@gmail.com>
2018-03-16 15:12:17 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/tecsup/labcalificado/MainActivity.java
@@ -0,0 +1,20 @@
+package pe.edu.tecsup.labcalificado;
+
+import android.media.session.MediaController;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    private VideoView videoView;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
